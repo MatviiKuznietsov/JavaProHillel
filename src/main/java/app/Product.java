7f01f4c5292f9de6113634c1f6252b3542a4ecf0
MTV <matveimtvcool@gmai.com>
2023-09-22 15:00:14 +0300

    HW_033 Done

---
+++ src/main/java/app/Product.java
@@ -0,0 +1,49 @@
+package app;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double cost;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
